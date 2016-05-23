0b96aba3061cece33f83350dab52d544fac38da3
Actumn <actumn814@gmail.com>
2016-05-24 06:42:22 +0900

    .

--- core/src/com/mygdx/game/map/Quadtree.java
+++ core/src/com/mygdx/game/map/Quadtree.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.map;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.object.Rectable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,7 @@ public class Quadtree {
     private int MAX_LEVELS = 5;
 
     private int level;
-    private List objects;
+    private List<Rectable> objects;
     private Rectangle bounds;
     private Quadtree[] nodes;
 
@@ -24,17 +25,17 @@ public class Quadtree {
         constructor
      */
     public Quadtree(int pLevel, Rectangle pBounds) {
-        level = pLevel;
-        objects = new ArrayList();
-        bounds = pBounds;
-        nodes = new Quadtree[4];
+        this.level = pLevel;
+        this.objects = new ArrayList<Rectable>();
+        this.bounds = pBounds;
+        this.nodes = new Quadtree[4];
     }
 
     /*
         Clear the quadtree
      */
     public void clear() {
-        objects.clear();
+        this.objects.clear();
 
         for (Quadtree qt: nodes) {
             if (qt != null) {
@@ -53,13 +54,74 @@ public class Quadtree {
         int x = (int)bounds.getX();
         int y = (int)bounds.getY();
 
-        nodes[0] = new Quadtree(level+1, new Rectangle(x + subWidth, y, subWidth, subHeight));
-        nodes[1] = new Quadtree(level+1, new Rectangle(x, y, subWidth, subHeight));
-        nodes[2] = new Quadtree(level+1, new Rectangle(x, y + subHeight, subWidth, subHeight));
-        nodes[3] = new Quadtree(level+1, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
+        int nextLevel = level + 1;
+        nodes[0] = new Quadtree(nextLevel, new Rectangle(x + subWidth, y, subWidth, subHeight));
+        nodes[1] = new Quadtree(nextLevel, new Rectangle(x, y, subWidth, subHeight));
+        nodes[2] = new Quadtree(nextLevel, new Rectangle(x, y + subHeight, subWidth, subHeight));
+        nodes[3] = new Quadtree(nextLevel, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
     }
 
     /*
-        Determine which node the object belongs to -1 means
+        Determine which node the object belongs to.
+         -1 means object cannot completely fit within a child node and is part
+         of the parent node
      */
+    private int getIndex(Rectangle pRect) {
+        int index = -1;
+        double verticalMidpoint = bounds.getX() + (bounds.getWidth() / 2);
+        double horizontalMidpoint = bounds.getY() + (bounds.getHeight() / 2);
+
+        // Object can completely fit whithin the top quadrants
+        boolean topQuadrant = (pRect.getY() < horizontalMidpoint
+                && pRect.getY() + pRect.getHeight() < horizontalMidpoint);
+        // Object can completely fit within the bottom quadrants
+        boolean bottomQuadrant = (pRect.getY() > horizontalMidpoint);
+
+        // Object can completely fit within the left quadrants
+        if (pRect.getX() < verticalMidpoint && pRect.getX() + pRect.getWidth() < verticalMidpoint) {
+            if (topQuadrant) {
+                index = 1;
+            }
+            else if (bottomQuadrant) {
+                index = 2;
+            }
+        }
+        // Object can completely fit within the right quadrants
+        else if (pRect.getX() > verticalMidpoint) {
+            if (topQuadrant) {
+                index = 0;
+            }
+            else if (bottomQuadrant) {
+                index = 3;
+            }
+        }
+
+        return index;
+    }
+
+    /*
+        insert the object into the quadtree.
+        If the node exceeds the capacity, it will split and add all objects
+        to their corresponding nodes
+     */
+    public void insert(Rectable rectable) {
+        Rectangle pRect = rectable.getRectangle();
+        if (nodes[0] != null) {
+            int index = getIndex(pRect);
+
+            if (index != -1) {
+                nodes[index].insert(rectable);
+
+                return;
+            }
+        }
+
+
+        objects.add(rectable);
+        if (objects.size() > MAX_OBJECTS && level < MAX_LEVELS) {
+
+        }
+
+    }
+
 }
